c3ed581c3f318c535015abbd80e6b07af4b3cb42
Daniel Dao <daniel.dao@chime.com>
2022-12-01 10:46:18 -0800

    working code

---
+++ spec/rubocop/cop/graphql/long_field_description_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::Cop::GraphQL::LongFieldDescription, :config do
+  context "when description is passed as argument" do
+    it "not registers an offense" do
+      expect_no_offenses(<<~RUBY)
+        class UserType < BaseType
+          field :first_name, String, "First name", null: true
+        end
+      RUBY
+    end
+  end
+
+  context "when description is too long" do
+    it "registers an offense" do
+      expect_offense(<<~RUBY)
+        class FooType < BaseType
+          field :first_name, String, "I am reaaaaaaaaaaaaaaaaallllllllllllllllllllllllllllllllllyyyyyyyyyyyyy long", null: true
+          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Long field description
+        end
+      RUBY
+    end
+  end
+end
